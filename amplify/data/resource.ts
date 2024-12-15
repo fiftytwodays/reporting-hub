68b563e124a2ac6b11d41c09195f2a1e09d369b4
abhirag.as <abhirag.as@ecsfin.com>
2024-12-15 23:08:15 +0530

    feat(Projects): Implement functionalities list-project,create-project,edit-project and delete-project

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -2,7 +2,6 @@ import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 import { listGroups } from "./groups/list-groups/resource";
 import { listUsers } from "./groups/list-users/resource";
-import { listProjects } from "./projects/list-projects/resource";
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -23,13 +22,6 @@ const schema = a.schema({
     .arguments({})
     .authorization((allow) => [allow.authenticated()])
     .handler(a.handler.function(listUsers))
-    .returns(a.json()),
-
-  listProjects: a
-    .mutation() // Use query since it's fetching data
-    .arguments({})
-    .authorization((allow) => [allow.authenticated()])
-    .handler(a.handler.function(listProjects)) // Define the handler function below
     .returns(a.json()),
 
   Todo: a
