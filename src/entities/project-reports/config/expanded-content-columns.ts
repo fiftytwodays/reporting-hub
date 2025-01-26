de5ae1192e57a5c7d9b9564161be0671d0f5e8c0
abhirag.as <am-on-a-mission@me.com>
2025-01-26 22:40:11 +0530

    feat(project-reports-listing): Update expanded content to show outcomes and additional activities for all months and years

--- src/entities/project-reports/config/expanded-content-columns.ts
+++ src/entities/project-reports/config/expanded-content-columns.ts
@@ -1,53 +1,68 @@
 export const currentMonth = [
-    {
-      title: "Goal",
-      dataIndex: "goal",
-      key: "goal",
-      width: "20%",
-    },
-    {
-      title: "Functional area",
-      dataIndex: "functionalArea",
-      key: "functionalArea",
-      width: "20%",
-    },
-    {
-      title: "Achieved",
-      dataIndex: "achieved",
-      key: "achieved",
-      width: "10%",
-    },
-    {
-      title: "Reason",
-      dataIndex: "reason",
-      key: "reason",
-      width: "25%",
-    },
-    {
-      title: "Comments",
-      dataIndex: "comments",
-      key: "comments",
-      width: "25%",
-    },
-  ]; 
-  
-  export const nextMonth = [
-    {
-      title: "Goal",
-      dataIndex: "goal",
-      key: "goal",
-      width: "30%",
-    },
-    {
-      title: "Functional area",
-      dataIndex: "functionalArea",
-      key: "functionalArea",
-      width: "30%",
-    },
-    {
-      title: "Comments",
-      dataIndex: "comments",
-      key: "comments",
-      width: "40%",
-    },
-  ];  
+  {
+    title: "Goal",
+    dataIndex: "goal",
+    key: "goal",
+    width: "20%",
+  },
+  {
+    title: "Major goal",
+    dataIndex: "majorGoal",
+    key: "majorGoal",
+    width: "10%",
+    render: (text: boolean) => (text ? "Yes" : "No"),
+  },
+  {
+    title: "Functional area",
+    dataIndex: "functionalArea",
+    key: "functionalArea",
+    width: "20%",
+  },
+  {
+    title: "Achieved",
+    dataIndex: "achieved",
+    key: "achieved",
+    width: "10%",
+    render: (text: boolean) => (text ? "Yes" : "No"),
+  },
+  {
+    title: "Reason",
+    dataIndex: "reason",
+    key: "reason",
+    width: "20%",
+  },
+  {
+    title: "Comments",
+    dataIndex: "comments",
+    key: "comments",
+    width: "20%",
+  },
+];
+
+export const nextMonth = [
+  {
+    title: "Goal",
+    dataIndex: "goal",
+    key: "goal",
+    width: "25%",
+  },
+  {
+    title: "Major goal",
+    dataIndex: "majorGoal",
+    key: "majorGoal",
+    width: "10%",
+    render: (text: boolean) => (text ? "Yes" : "No"),
+  },
+  {
+    title: "Functional area",
+    dataIndex: "functionalArea",
+    key: "functionalArea",
+    width: "25%",
+  },
+  {
+    title: "Comments",
+    dataIndex: "comments",
+    key: "comments",
+    width: "40%",
+  },
+];
