1dc38850e9b7632cf7a438b32995e9ec88b19ce8
abhirag.as <am-on-a-mission@me.com>
2025-04-03 03:32:28 +0530

    draft

---
+++ src/feature/create-monthly-form/api/functional-area-options.ts
@@ -0,0 +1,58 @@
+import type { Schema } from "@root/amplify/data/resource";
+import useSWR, { mutate } from "swr";
+import { generateClient } from "aws-amplify/data";
+
+interface FetchOptions {
+  condition: boolean;
+}
+
+interface ApiResponse {
+  FunctionalAreas: FunctionalArea[];
+}
+
+export interface FunctionalArea {
+  value: string;
+  label: string;
+  // location: String;
+}
+
+export default function useFunctionalAreaList({
+  condition = true,
+}: FetchOptions) {
+  const client = generateClient<Schema>();
+
+  const fetcher = async () => {
+    const response = await client.models.FunctionalArea.list();
+    if (response?.data) {
+      const functionalAreas = await Promise.all(
+        response.data.map(async (functionalArea) => {
+          return {
+            label: functionalArea.name ?? "",
+            value: functionalArea.id ?? "",
+          };
+        })
+      );
+
+      const apiResponse: ApiResponse = {
+        FunctionalAreas: functionalAreas,
+      };
+
+      return apiResponse; // Return the apiResponse instead of response.data to include functionalArea
+    }
+    return null;
+  };
+
+  const { data, isLoading, error } = useSWR(
+    condition ? ["api/functionalAreas"] : null,
+    fetcher,
+    {
+      keepPreviousData: true,
+    }
+  );
+
+  return {
+    functionalAreasData: data?.FunctionalAreas,
+    isFunctionalAreaTypesDataLoading: isLoading,
+    functionalAreaTypesDataError: error,
+  };
+}
